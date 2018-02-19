7b0b11373609dc6ed233f94a5f96113fae2aa137
cadecahalan <cadecahalan@berkeley.edu>
2018-02-19 14:30:56 -0800

    Update water-arrays.js

--- water-arrays.js
+++ water-arrays.js
@@ -1,4 +1,4 @@
-var dropX = 230
+var dropX = [];
 var dropY = [];
 var speed = [];
 
@@ -8,7 +8,8 @@ function setup() {
   
   for (var i = 0; i < 10; i = i + 1) {
     dropY[i] = random(220, height);
-    speed[i] = random(1, 5);
+    speed[i] = random(2, 5);
+    dropX[i] = 230;
   }
 }
   
@@ -19,12 +20,22 @@ function draw() {
   rect(0, 200, 230, 20);
   
   for (var i = 0; i < 10; i = i + 1) {
-     ellipse(230, dropY[i], 10);
+     ellipse(dropX[i], dropY[i], 10);
      
      dropY[i] = dropY[i] + speed[i];
      
+    if (speed[i] < 3){
+      dropX[i] = dropX[i] + 0.5;
+     }
+    
+    if (speed[i] > 4){
+      dropX[i] = dropX[i] + 3;
+     }
+    
      if (dropY[i] > height) {
        dropY[i] = 220;
+       dropX[i] = 230;
      }
+    
   }
 }
